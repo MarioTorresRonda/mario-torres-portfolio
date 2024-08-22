7a7138af760afcc933c8a1fe5c123baedd5be4eb
MarioTorresRonda <mariotorresrma@gmail.com>
2024-08-22 03:22:51 +0200

    Responsive navbar

--- components/fragments/NavBar.jsx
+++ components/fragments/NavBar.jsx
@@ -1,23 +1,34 @@
+'use client'
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import DinamicColors from "./DinamicColors"
 import FAI from "./FAI"
 import NavBarItem from "./NavBarItem"
 import menus from "@/data/navBar"
 import { faBarChart as icon } from "@fortawesome/free-regular-svg-icons"
+import { useState } from "react"
 
 export default function NavBar() {
+
+    const [show, setShow] = useState(false);
+
+    const hiddenClasses = "max-h-0 scale-y-0 opacity-0";
+    const showClasses = "max-h-[140px] scale-y-100 opacity-100";
+    //Classes that surpass the hidden state when the site is bigger and is not needed
+    const forceClasses = "sm:max-h-full sm:h-full sm:scale-y-100 sm:w-auto "
+
     return (
-        <div className="flex gap-2 mx-4 h-[10vh] ">
-            <div className="flex-1 mt-1">
+        <div className="flex flex-wrap gap-2 mx-4 min-h-[10vh]">
+            <div className="flex-1 mt-1 sm:ml-4 md:ml-10 ">
                 <DinamicColors />
             </div>
-            <div className="visible sm:invisible self-center">
+            <div className="block mt-1 self-end sm:hidden" onClick={() => { setShow( !show ) }}>
                 <FontAwesomeIcon
                     icon={icon}
-                    className={`text-[--myColorStartSimple] w-[40px] h-[40px] transition-transform ease-in-out hover:scale-[1.2] hover:text-slate-700 dark:hover:text-stone-500 `}
+                    className={`text-[--myColorStartSimple] w-[40px] h-[40px] transition-transform ease-in-out hover:scale-[1.2] hover:text-[--myColorEndSimple] `}
                 />
             </div>
-            <div className="hidden sm:flex flex-col sm:flex-row justify-end gap-6 mr-4 md:mr-10">
+            <div className={`${ show ? showClasses : hiddenClasses } ${forceClasses} w-full transition-all flex justify-end self-center flex-col sm:flex-row sm:gap-6 sm:mr-4 md:mr-10`} >
                 { Object.keys( menus ).map( (key) => { return <NavBarItem key={key} newMenu={menus[key]} /> } )  }
             </div>
         </div>
--- components/fragments/NavBarItem.jsx
+++ components/fragments/NavBarItem.jsx
@@ -16,10 +16,12 @@ export default function NavBarItem( { newMenu } ) {
         navigate( newMenu );
     }
 
-    return <button className="flex h-[100%] items-center z-10" onClick={() => onHandleClick()}>
-        <div className=" flex flex-col items-center">
-            <p> <span className="color-dinamico-texto" > {newMenu.num}. </span> {menuText} </p>
-            { menu && newMenu.symbol == menu.symbol && <div className="bg-slate-400 dark:bg-slate-700 h-1 w-[120%]" ></div> }
-        </div>
-    </button>
+    return (
+        <div className="flex h-[100%] p-2 items-center z-10 justify-end" >
+            <button className="flex flex-row sm:flex-col items-center" onClick={() => onHandleClick()}>
+                <p> <span className="color-dinamico-texto" > {newMenu.num}. </span> {menuText} </p>
+                { menu && newMenu.symbol == menu.symbol && <div className="bg-slate-400 dark:bg-slate-700 ml-3 w-1 h-8 sm:ml-0 sm:h-1 sm:w-[120%]" ></div> }
+            </button>
+        </div> 
+    )
 }
